5e0d135f9abd8cfc4191f03c6733f424f5fd7206
RadX <ralobba@gmail.com>
2019-10-31 21:26:33 +1100

    Case sensitive extensions accommodated

--- run.sh
+++ run.sh
@@ -22,6 +22,20 @@ do
 	mv "$f" $dateFolder    
 done
 
+for f in *.MOV
+do
+    dateFolder=$(exiftool -CreateDate "$f" |awk '{print $4 "/"}' |sed -e 's/:/-/g')
+    mkdir -p $dateFolder
+	mv "$f" $dateFolder    
+done
+
+for f in *.mov
+do
+    dateFolder=$(exiftool -CreationDate "$f" |awk '{print $4 "/"}' |sed -e 's/:/-/g')
+    mkdir -p $dateFolder
+	mv "$f" $dateFolder    
+done
+
 for f in *.MP4
 do
     dateFolder=$(exiftool -CreationDate "$f" |awk '{print $4 "/"}' |sed -e 's/:/-/g')
@@ -32,6 +46,20 @@ done
 for f in *.mp4
 do
     dateFolder=$(exiftool -CreateDate "$f" |awk '{print $4 "/"}' |sed -e 's/:/-/g')
+    mkdir -p $dateFolder
+	mv "$f" $dateFolder    
+done
+
+for f in *.MP4
+do
+    dateFolder=$(exiftool -CreateDate "$f" |awk '{print $4 "/"}' |sed -e 's/:/-/g')
+    mkdir -p $dateFolder
+	mv "$f" $dateFolder    
+done
+
+for f in *.mp4
+do
+    dateFolder=$(exiftool -CreationDate "$f" |awk '{print $4 "/"}' |sed -e 's/:/-/g')
     mkdir -p $dateFolder
 	mv "$f" $dateFolder    
 done
